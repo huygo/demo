ff08be3933fcce873115c2e2e31e8c3bb9735f8e
huygo <quanghuy.pt97@gmail.com>
2020-10-07 21:44:37 +0700

    test branch

--- index.php
+++ index.php
@@ -5,6 +5,11 @@
 		require 'libs/model.php';
 		require 'libs/controller.php';
 
+		// require 'libs/startup.php';
+		// require 'cms/libs/database.php';
+		// require 'cms/libs/functions.php';
+		// require 'libs/model.php';
+		// require 'libs/controller.php';
 		
 		
 ?>
