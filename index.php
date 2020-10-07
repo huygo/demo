89c46412e66c829586f22fb26f94d535bbd02aa6
huygo <quanghuy.pt97@gmail.com>
2020-10-07 21:31:18 +0700

    resset

--- index.php
+++ index.php
@@ -5,15 +5,6 @@
 		require 'libs/model.php';
 		require 'libs/controller.php';
 
-		// require 'libs/startup.php';
-		// require 'cms/libs/database.php';
-		// require 'cms/libs/functions.php';
-		// require 'libs/model.php';
-		// require 'libs/controller.php';
-		// require 'libs/startup.php';
-		// require 'cms/libs/database.php';
-		// require 'cms/libs/functions.php';
-		// require 'libs/model.php';
-		// require 'libs/controller.php';
+		
 		
 ?>
