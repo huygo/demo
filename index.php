c339a1bc2f0e5cc22c676f2c24c827a44e7c8d92
huygo <quanghuy.pt97@gmail.com>
2020-10-07 21:20:42 +0700

    code loi

--- index.php
+++ index.php
@@ -3,5 +3,17 @@
 		require 'cms/libs/database.php';
 		require 'cms/libs/functions.php';
 		require 'libs/model.php';
-		require 'libs/controller.php'
+		require 'libs/controller.php';
+
+		// require 'libs/startup.php';
+		// require 'cms/libs/database.php';
+		// require 'cms/libs/functions.php';
+		// require 'libs/model.php';
+		// require 'libs/controller.php';
+		// require 'libs/startup.php';
+		// require 'cms/libs/database.php';
+		// require 'cms/libs/functions.php';
+		// require 'libs/model.php';
+		// require 'libs/controller.php';
+		
 ?>
